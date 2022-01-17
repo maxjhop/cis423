974e1223dd68813f603f6ea7e6c455e5b7bc84f7
maxjhop <57085083+maxjhop@users.noreply.github.com>
2022-01-17 14:46:35 -0800

    testing

--- library.py
+++ library.py
@@ -28,34 +28,6 @@ class DropColumnsTransformer(BaseEstimator, TransformerMixin):
     result = self.transform(X)
     return result
   
-class OHETransformer(BaseEstimator, TransformerMixin):
-  def __init__(self, target_column, dummy_na=False, drop_first=True):  
-    self.target_column = target_column
-    self.dummy_na = dummy_na
-    self.drop_first = drop_first
-    
-  
-  #fill in the rest below
-  def fit(self, X, y = None):
-    print("Warning: MappingTransformer.fit does nothing.")
-    return X
-
-  def transform(self, X):
-    X_ = X.copy()
-    X_ = pd.get_dummies(X,
-                                prefix=self.target_column,    #your choice
-                                prefix_sep='_',     #your choice
-                                columns=[self.target_column],
-                                dummy_na=self.dummy_na,    #will try to impute later so leave NaNs in place
-                                drop_first=self.drop_first    #will drop Belfast and infer it
-                                )
-    
-    return X_
-
-  def fit_transform(self, X, y = None):
-    result = self.transform(X)
-    return result
-  
 #This class maps values in a column, numeric or categorical.
 class MappingTransformer(BaseEstimator, TransformerMixin):
   
